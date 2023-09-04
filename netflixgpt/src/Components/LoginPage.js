c8fc9bef7a13b07501230b5a1412622612a1065d
Kaushik <kmzdr1@gmail.com>
2023-09-04 16:53:01 +0530

    Rectification BackgroundImage through img element

--- netflixgpt/src/Components/LoginPage.js
+++ netflixgpt/src/Components/LoginPage.js
@@ -4,8 +4,9 @@ import Header from './Header'
 const LoginPage = () => {
   return (
     
-    <div className="bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_medium.jpg')] bg-cover bg-center bg-no-repeat" >
+    <div >
       <Header/>
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="Netflix background"/>
       <form>
         <p>Sign In</p>
         <input type="email" placeholder='Enter your Email'/>
