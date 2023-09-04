0d9db621a301afed40311000c62d90c9be4db072
Kaushik <kmzdr1@gmail.com>
2023-09-04 18:17:21 +0530

    Feat:Login Form Css

--- netflixgpt/src/Components/LoginPage.js
+++ netflixgpt/src/Components/LoginPage.js
@@ -6,12 +6,13 @@ const LoginPage = () => {
     
     <div >
       <Header/>
-      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="Netflix background"/>
-      <form>
-        <p>Sign In</p>
-        <input type="email" placeholder='Enter your Email'/>
-        <input type="password" placeholder="Enter your password"/>
-        <button>Sign In</button>
+      <img  className="relative" src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="Netflix background"/>
+      <form className="absolute z-40 w-[35%] p-16 top-32 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-80">
+        <p className="text-3xl text-white font-bold py-5">Sign In</p>
+        <input className="w-full p-3 my-2 rounded-sm bg-gray-600" type="email" placeholder='Enter your Email'/>
+        <input className="w-full p-3 my-2 rounded-sm bg-gray-600" type="password" placeholder="Enter your password"/>
+        <button className="w-full p-3 my-8 text-white rounded-sm  bg-red-700">Sign In</button>
+        <p className="text-gray-400">New to Netflix? <span className="text-white cursor-pointer">Sign Up Now</span></p>
       </form>
     </div>
     
